522d6e743abb2e68830ad701faf938b7019c87dc
kaseycolian <35547685+kaseycolian@users.noreply.github.com>
2018-02-05 00:33:36 -0500

    Delete VirtualPetShelterAppRedux.java

--- src/main/java/VirtualPetShelterAppRedux.java
+++
@@ -1,9 +0,0 @@
-
-public class VirtualPetShelterAppRedux {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//add with same method used in bank with new PetShelterAPpRedux = new with entire string of levels
-	}
-
-}
